9cf2dcab1e14ff17be8e58e99c8ac8cccbcaeb48
Aaron Hoskins <aaron.hoskins@mobileappscompany.us>
2018-11-20 19:05:16 -0500

    Initial commit

---
+++ app/src/main/java/com/mobileapps/fileio/MainActivity.java
@@ -0,0 +1,71 @@
+package com.mobileapps.fileio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText textmsg;
+    TextView tvTextResults;
+    static final int READ_BLOCK_SIZE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textmsg= findViewById(R.id.editText1);
+        tvTextResults = findViewById(R.id.textView2);
+    }
+    // write text to file
+    public void WriteBtn(View v) {
+        // add-write text into file
+        try {
+            FileOutputStream fileout=openFileOutput("mytextfile.txt", MODE_PRIVATE);
+            OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
+            outputWriter.write(textmsg.getText().toString());
+            outputWriter.close();
+
+            //display file saved message
+            Toast.makeText(this, "File saved successfully!", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Log.e("TAG", "WriteBtn: ", e);
+        }
+    }
+
+    // Read text from file
+    public void ReadBtn(View v) {
+        //reading text from file
+        try {
+            FileInputStream fileIn=openFileInput("mytextfile.txt");
+            InputStreamReader InputRead= new InputStreamReader(fileIn);
+
+            char[] inputBuffer= new char[READ_BLOCK_SIZE];
+            String s="";
+            int charRead;
+
+            while ((charRead=InputRead.read(inputBuffer))>0) {
+                // char to string conversion
+                String readstring=String.copyValueOf(inputBuffer,0,charRead);
+                s +=readstring;
+            }
+            InputRead.close();
+            tvTextResults.setText(s);
+            //Toast.makeText(getBaseContext(), s,Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
